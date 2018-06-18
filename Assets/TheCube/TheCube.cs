d2d2f883cf7b7799d8a5da56ced951414e433caf
DESKTOP-4GST3TD\okayama <shuhei-o@digirea.com>
2018-06-18 16:54:33 +0900

    init

---
+++ Assets/TheCube/TheCube.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheCube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("aaaa");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("space")){
+			Debug.Log("jjjjjjjjjj");
+			Debug.Log("++++++");
+		}
+	}
+}
